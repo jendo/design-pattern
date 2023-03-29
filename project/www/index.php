b63d8678de79a0a2566474fbb28e5a8528ea4716
Michal <michal.jenis@kosik.cz>
2023-03-29 16:13:58 +0200

    Create and run application

--- project/www/index.php
+++ project/www/index.php
@@ -2,10 +2,16 @@
 
 declare(strict_types=1);
 
+use App\Application;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 try {
     $container = App\Bootstrap::boot();
+
+    /** @var Application $application */
+    $application = $container->getByType(Application::class);
+    $application->run();
 } catch (Exception $e) {
     exit($e->getMessage());
 }
