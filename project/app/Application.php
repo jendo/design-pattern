c9fb0d60ac70239634b63a234df79da804956db7
Michal <michal.jenis@kosik.cz>
2023-03-29 17:06:16 +0200

    Add singleton pattern

--- project/app/Application.php
+++ project/app/Application.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\DesignPattern\Creational\Singleton;
+use Tracy\Debugger;
+
 class Application
 {
+
     public function run(): void
     {
+        /**
+         * Singleton example
+         */
+        $s1 = Singleton::getInstance();
+        $s2 = Singleton::getInstance();
 
+        Debugger::dump($s1);
+        Debugger::dump($s2);
+        Debugger::dump($s1 === $s2);
     }
 }
